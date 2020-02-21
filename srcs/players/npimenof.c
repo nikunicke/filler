1e4677a185bab8535112d71c17042a2958ceda94
Niklas Pimenoff <npimenof@c2r4p11.hive.fi>
2020-02-21 20:18:42 +0200

    separated mathematical calculation to its own function

--- srcs/players/npimenof.c
+++ srcs/players/npimenof.c
@@ -6,31 +6,40 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 11:46:06 by npimenof          #+#    #+#             */
-/*   Updated: 2020/02/20 17:04:54 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/02/21 19:43:14 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "filler.h"
 
-int		possible_coordinates(char *map_index, char *piece_index, t_data *game)
+int		calculate_offset(char *piece_index, t_data *game)
+{
+	int		offset;
+	int		piece_i;
+	int		piece_y;
+
+	piece_y = game->piece->y;
+	piece_i = piece_index - game->piece->area;
+	offset = game->map->y * (piece_i / piece_y) + (piece_i % piece_y);
+	return (offset);
+}
+
+int		coordinates(char *map_index, char *piece_index, t_data *game, int *arr)
 {
 	int			offset;
 	int			crd;
 	int			map_i;
-	int			piece_i;
 	char		*copy;
 
 	if ((copy = ft_strchr(piece_index + 1, '*')))
-		possible_coordinates(map_index, copy, game);
-	piece_i = piece_index - game->piece->area;
+		possible_coordinates(map_index, copy, game); 
 	map_i = map_index - game->map->area;
-	crd = map_i - (game->map->y * (piece_i / game->piece->y) + (piece_i % game->piece->y)); // create funktion of this?
+	crd = map_i - calculate_offset(piece_index, game);
 	piece_index = game->piece->area;
 	while ((piece_index = ft_strchr(piece_index, '*')))
 	{
-		piece_i = piece_index - game->piece->area;
-		offset = game->map->y * (piece_i / game->piece->y) + (piece_i % game->piece->y);
+		offset = calculate_offset(piece_index, game);
 		if (crd + offset == map_i && piece_index++)
 			continue ;
 		if (crd + offset < 0 || crd + offset > (game->map->x * game->map->y) ||
@@ -59,11 +68,12 @@ int		available_coordinates(t_data *game)
 
 int		npimenof(t_data *game)
 {
-	int		i;
-	int		j;
+	// int		i;
+	// int		j;
 
-	i = ft_strcspn(game->map->area, "oO");
-	j = ft_strcspn(game->map->area, "xX");
+	// i = ft_strcspn(game->map->area, "oO");
+	// j = ft_strcspn(game->map->area, "xX");
 	available_coordinates(game);
+	
 	return (0);
 }
