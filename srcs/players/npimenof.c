eeef3ccee84ba8cba34e2acf3bda00408d0b5348
Niklas Pimenoff <npimenof@c2r4p11.hive.fi>
2020-02-23 18:06:31 +0200

    fixed function to store coordinates. Was missin a malloc and some if rules. This structure needs a freeing function.

--- srcs/players/npimenof.c
+++ srcs/players/npimenof.c
@@ -6,7 +6,7 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 11:46:06 by npimenof          #+#    #+#             */
-/*   Updated: 2020/02/22 19:17:28 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/02/23 18:04:39 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,20 @@ int		calculate_offset(char *piece_index, t_data *game)
 	piece_y = game->piece->y;
 	piece_i = piece_index - game->piece->area;
 	offset = game->map->y * (piece_i / piece_y) + (piece_i % piece_y);
+	// printf("offset: %d\n", offset);
 	return (offset);
+}
+
+int		check_row(int crd, int offset, t_data *game)
+{
+	int		map_row;
+	int		limit;
+
+	map_row = (crd + offset) / game->map->y;
+	limit = (crd / game->map->y) + (game->piece->x - 1);
+	if (map_row > limit)
+		return (1);
+	return (0);
 }
 
 t_list	*coordinates(char *map_index, char *piece_index, t_data *game)
@@ -34,8 +47,6 @@ t_list	*coordinates(char *map_index, char *piece_index, t_data *game)
 	t_list		*item;
 
 	item = ft_lstnew(NULL, 0);
-	// printf("Item addr: %p\n", &item);
-
 	if ((copy = ft_strchr(piece_index + 1, '*')))
 		item->next = coordinates(map_index, copy, game);
 	map_i = map_index - game->map->area;
@@ -47,17 +58,18 @@ t_list	*coordinates(char *map_index, char *piece_index, t_data *game)
 		if (crd + offset == map_i && piece_index++)
 			continue ;
 		if (crd + offset < 0 || crd + offset > (game->map->x * game->map->y) ||
-			game->map->area[crd + offset] != '.')
+			game->map->area[crd + offset] != '.' || check_row(crd, offset, game))
 			return (item);
 		piece_index++;
 	}
 	printf("Piece fits at %d %d\n", crd / game->map->y, crd % game->map->y);
-	item->content = &crd;
+	item->content = malloc(sizeof(int));
+	ft_memcpy(item->content, &crd, sizeof(int));
 	item->content_size = sizeof(int);
 	return (item);
 }
 
-int		available_coordinates(t_data *game)
+t_list	*available_coordinates(t_data *game)
 {
 	char	*map_index;
 	char	*piece_index;
@@ -67,35 +79,28 @@ int		available_coordinates(t_data *game)
 
 	i = 0;
 	head = ft_lstnew(NULL, sizeof(t_list));
-	// printf("%p\n", &head);
 	tmp = head;
 	map_index = game->map->area;
 	piece_index = ft_strchr(game->piece->area, '*');
 	while ((map_index = ft_strchr(map_index, 'X')))
 	{
 		head->next = ft_lstnew((void *)coordinates(map_index, piece_index, game), sizeof(t_list));
-		// printf("Head->content [ element: %d, addr: %p ]\n", i, &head->content);
 		map_index++;
 		head = head->next;
 		i++;
-		printf("\n");
 	}
 	head = tmp;
-	// printf("\n%p\n", &head);
-	// printf("\n%p\n", &tmp);
-	printf("\n\n\n");
-	i = 0;
+	return (head);
+}
+
+void	traverse_linked_linked_list(t_list *head)
+{
 	t_list	*item;
 	int		a;
+	
 	while (head)
 	{
-		// printf ("%p\n", &head->content);
-		printf("Head->content [ element: %d, addr: %p ]\n", i, &head->content);
 		item = (t_list *)head->content;
-		// if (((t_list *)&head->content)->content != NULL)
-		// {
-		// 	printf("%d\n\n", *(int *)((t_list *)&head->content)->content);
-		// }
 		while (item)
 		{
 			if (item->content == NULL)
@@ -104,24 +109,19 @@ int		available_coordinates(t_data *game)
 				continue;
 			}
 			a = *((int *)item->content);
-			printf("%d, ", a);
+			printf("%d ", a);
 			item = item->next;
 		}
-		printf("\n\n");
-		i++;
+		printf("\n");
 		head = head->next;
 	}
-	return (0);
 }
 
 int		npimenof(t_data *game)
 {
-	// int		i;
-	// int		j;
+	t_list	*coordinates;
 
-	// i = ft_strcspn(game->map->area, "oO");
-	// j = ft_strcspn(game->map->area, "xX");
-	available_coordinates(game);
-	
+	coordinates = available_coordinates(game); // remember to free this shit
+	traverse_linked_linked_list(coordinates);
 	return (0);
 }
