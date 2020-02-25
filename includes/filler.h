8deed6caa6990d5b93b8fbfb33f1392b212e3e41
Niklas Pimenoff <npimenof@c3r4p2.hive.fi>
2020-02-25 19:06:07 +0200

    added new functions to header. Mainly functions for the point struct

--- includes/filler.h
+++ includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 13:37:49 by npimenof          #+#    #+#             */
-/*   Updated: 2020/02/19 10:59:46 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/02/25 18:13:08 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # define FILLER_H
 # define PLAYER npimenof
 
-int				fd;
+// int				fde;
 
 typedef struct	s_player 
 {
@@ -44,12 +44,24 @@ typedef struct	s_data
 	t_grid		*piece;
 }				t_data;
 
+typedef struct	s_point
+{
+	int			x;
+	int			y;
+}				t_point;
+
 t_player		*new_player(void);
 t_grid			*new_grid(char *identifier);
 t_data			*new_game(t_player *(*new_player)(), t_grid *(*new_grid)());
 
-void			update_area(t_grid *map);
+t_point			*new_point(int x, int y);
+t_point			set_point(int x, int y);
+t_point			point_addition(t_point *sum, t_point *point);
+int				point_to_index(t_point point, t_grid *map);
+
+void			update_area(t_grid **map);
 
 int				npimenof(t_data *game);
 
+int				ft_log(char *msg, ...);
 #endif
