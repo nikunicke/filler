e85cd54c0ff09fadb3915e2880f99d07c27a864b
Niklas Pimenoff <pimenoff.niklas@gmail.com>
2020-03-12 17:09:35 +0200

    new libft func

--- libft/ft_lstpop.c
+++ libft/ft_lstpop.c
@@ -6,7 +6,7 @@
 /*   By: npimenof <npimenof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 13:32:29 by npimenof          #+#    #+#             */
-/*   Updated: 2020/03/08 13:32:53 by npimenof         ###   ########.fr       */
+/*   Updated: 2020/03/08 18:10:02 by npimenof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@ void	ft_lstpop(t_list **head)
 
 	tmp = *head;
 	*head = tmp->next;
-	ft_lstdelone(&tmp, ft_del_lstcontent);
+	free(tmp->content);
+	tmp->content = NULL;
+	free(tmp);
+	tmp = NULL;
 }
